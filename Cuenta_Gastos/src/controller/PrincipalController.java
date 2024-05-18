3828e94da030db5b2d60fd7c147d69acb56728f7
Plant300 <denisroda3@gmail.com>
2024-05-18 22:57:12 +0200

    Revert "Iniciar Sesión"
    This reverts commit 2f164d0bb75df34ff02e81a53bf7e43aa82839f5.

--- Cuenta_Gastos/src/controller/PrincipalController.java
+++ Cuenta_Gastos/src/controller/PrincipalController.java
@@ -6,13 +6,7 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.text.Text;
 
 /**
  * FXML Controller class
@@ -20,19 +14,6 @@ import javafx.scene.text.Text;
  * @author jiaji
  */
 public class PrincipalController implements Initializable {
-
-    @FXML
-    private HBox hBox;
-    @FXML
-    private TextField User;
-    @FXML
-    private PasswordField Pass;
-    @FXML
-    private Text errCon;
-    @FXML
-    private Text Registro;
-    @FXML
-    private Button inicio;
 
     /**
      * Initializes the controller class.
