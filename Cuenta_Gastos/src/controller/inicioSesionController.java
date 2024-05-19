349eb9e23a6570b7b1b1fa6d4f0d71586eb8b64e
Plant300 <denisroda3@gmail.com>
2024-05-19 13:34:46 +0200

    Cam

--- Cuenta_Gastos/src/controller/inicioSesionController.java
+++ Cuenta_Gastos/src/controller/inicioSesionController.java
@@ -4,9 +4,11 @@
  */
 package controller;
 
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -18,11 +20,12 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafxmlapplication.JavaFXMLApplication; 
-import model.Acount.*;
+import model.Acount;
+import model.AcountDAOException;
+
 /**
  * FXML Controller class
- *
+ * 
  * @author jiaji
  */
 public class inicioSesionController implements Initializable {
@@ -43,36 +46,36 @@ public class inicioSesionController implements Initializable {
     /**
      * Initializes the controller class.
      */
-    
-    private void pulsadoIniciar(ActionEvent event) throws IOException {
-        if(User.getText()== null || Password.getText()==null){
-            errCon.setText("Faltan campos por completar");
-        }else if(logInUserByCredentials(User.getText(),Password.getText())==false){
-            errCon.setText("No existe el usuario");
-        }else{
-            FXMLLoader loader =new FXMLLoader(getClass().getResource("homeScreen.fxml"));
-            Parent root=loader.load();
-            iniciar.getScene().getWindow().hide();
-            Scene scene= new Scene(root);
-            Stage stage=new Stage();
-            stage.setScene(scene);
-            stage.showAndWait();
-        }
-    }
-    
-    private void irRegistro(MouseEvent event) throws IOException{
-        FXMLLoader loader =new FXMLLoader(getClass().getResource("Registro.fxml"));
-        Parent root=loader.load();
-        Registro.getScene().getWindow().hide();
-        Scene scene= new Scene(root);
-        Stage stage=new Stage();
-        stage.setScene(scene);
-        stage.showAndWait();
-    }
-    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-        
-    }    
+        // Initialization logic if needed
+    }
+
+    @FXML
+    private void pulsadoIniciar(ActionEvent event) throws IOException, AcountDAOException {
+        if (User.getText().isEmpty() || Password.getText().isEmpty()) {
+            errCon.setText("Faltan campos por completar");
+        } else if (!Acount.getInstance().logInUserByCredentials(User.getText(), Password.getText())) {
+            errCon.setText("No existe el usuario");
+        } else {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/path/to/homeScreen.fxml"));
+            Parent root = loader.load();
+            iniciar.getScene().getWindow().hide();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+            ((Stage) iniciar.getScene().getWindow()).close();
+        }
+    }
+
+    @FXML
+    private void irRegistro(MouseEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/path/to/Registro.fxml"));
+        Parent root = loader.load();
+        Registro.getScene().getWindow().hide();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.show();
+        ((Stage) Registro.getScene().getWindow()).close();
+    }
 }
